3d0f667e972b7207d29ca2f1bd1756e24aa407a1
Marcin <mvrrrcin@gmail.com>
2019-08-29 13:32:18 +0200

    python exercises

--- Ex. 1.py
+++ Ex. 1.py
@@ -2,7 +2,7 @@
 # Practicepython.org Ex. 1 - Character Input
 
 name = input("Podaj swoje imie:")
-age = int(input("Ile masz lat:"))
-year = str((2019 - age)+100)
-print(name + ", 100 lat skonczysz w " + year + " roku.")
-
+age = int(input("Ile masz lat?:"))
+year = int(input("Podaj aktualny rok:"))
+death = str(year - age + 100)
+print(name, "100 lat skonczysz w",death, "roku.")
---
+++ Ex.3.py
@@ -0,0 +1,7 @@
+# Marcin Piskuła
+# Practicepython.org Ex. 3 - List Less Than Ten
+
+list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for b in list:
+    if b < 5:
+        print(b)
